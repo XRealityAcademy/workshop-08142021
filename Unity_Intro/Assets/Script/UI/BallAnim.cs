fc7a63eee0060e5dcd11d97192c4a00678d10098
hummingbirdsday <hummingbirdsday@gmail.com>
2021-09-10 20:38:45 -0700

    UI design

---
+++ Unity_Intro/Assets/Script/UI/BallAnim.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallAnim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.LeanScale(Vector3.one, 2f).setEaseOutBounce().setLoopPingPong();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
